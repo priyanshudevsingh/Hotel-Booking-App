47c872d269f390c8c88cccd1321d9136e7940113
Priyanshu Singh <therealgrindin.001@gmail.com>
2024-01-12 23:29:54 +0530

    Deployment Fix 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import NavBar from "./Components/Navbar.js";
+import NavBar from "./Components/NavBar.js";
 import Home from "./Components/Home.js";
 import HotelListings from "./Components/HotelListings.js";
 import HotelDetails from "./Components/HotelDetails.js";
