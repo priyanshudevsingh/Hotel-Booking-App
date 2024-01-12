bcab00210734049b838df2be39a1c1d3217ec9bf
Priyanshu Singh <therealgrindin.001@gmail.com>
2024-01-12 22:59:58 +0530

    Deployment Bug Fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import NavBar from "./Components/Navbar";
-import Home from "./Components/Home";
-import HotelListings from "./Components/HotelListings";
-import HotelDetails from "./Components/HotelDetails";
+import NavBar from "./Components/Navbar.js";
+import Home from "./Components/Home.js";
+import HotelListings from "./Components/HotelListings.js";
+import HotelDetails from "./Components/HotelDetails.js";
 
 function App() {
   return (
